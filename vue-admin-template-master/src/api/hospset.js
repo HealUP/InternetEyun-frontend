9ebc5ded394f011d30f952b3f15202f1b9899bde
HealUp <1571826275@qq.com>
2023-02-20 18:01:59 +0800

    添加：完成医院信息数据显示、删除医院信息功能

--- vue-admin-template-master/src/api/hospset.js
+++ vue-admin-template-master/src/api/hospset.js
@@ -9,10 +9,19 @@ import request from '@/utils/request'
 //     })
 //   }
 // }
+
+// 查询医院设置列表
 export function getHospSetList(current, limit, searchObj) {
   return request({
     url: `/admin/hosp/hospitalSet/findPageHospSet/${current}/${limit}`,
     method: 'post',
     data: searchObj // 使用json格式传递值
+  })
+}
+// 删除医院设置
+export function deleteHospSet(id) {
+  return request({
+    url: `/admin/hosp/hospitalSet/${id}`,
+    method: `delete`
   })
 }
--- vue-admin-template-master/src/views/hospset/list.vue
+++ vue-admin-template-master/src/views/hospset/list.vue
@@ -1,12 +1,76 @@
+<!--定义banner列表-->
 <template>
   <div class="app-container">
     医院设置列表
+    <el-form :inline="true" class="demo-form-inline">
+      <el-form-item label="医院名称">
+        <el-input v-model="searchObj.hosname" placeholder="医院名称"/>
+      </el-form-item>
+      <el-form-item label="医院编号">
+        <el-input v-model="searchObj.hoscode" placeholder="医院编号"/>
+      </el-form-item>
+      <el-button type="primary" icon="el-icon-search" @click="getList()">查询</el-button>
+    </el-form>
+    <el-table
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+        prop="id"
+        label="序号"
+        width="50"/>
+      <el-table-column
+        prop="hosname"
+        label="医院名称"
+        width="180"/>
+      <el-table-column
+        prop="hoscode"
+        label="医院编号"/>
+      <el-table-column
+        prop="apiUrl"
+        label="api基础路径"
+        width="200"/>
+      <el-table-column
+        prop="contactsName"
+        label="联系人姓名"/>
+      <el-table-column
+        prop="contactsPhone"
+        label="联系人手机"/>
+      <el-table-column
+        label="状态"
+        width="80">
+        <template slot-scope="scope">
+          {{ scope.row.status === 1 ? '可用' : '不可用' }}
+        </template>
+      </el-table-column>
+      <!-- 删除列 -->
+      <el-table-column
+        label="操作"
+        width="100"
+        align="center">
+        <template slot-scope="scope">
+          <el-button
+            type="danger"
+            size="mini"
+            icon="el-icon-delete"
+            @click="removeDataById(scope.row.id)"/>
+        </template>
+      </el-table-column>
+      <!-- 删除按钮 -->
+    </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      :current-page.sync="current"
+      :page-size="limit"
+      :total="total"
+      style="padding: 30px 0; text-align: center;"
+      layout="prev, pager, next, jumper"
+      @current-change="getList"/>
   </div>
 </template>
 
 <script>
 // TODO 引入接口定义的文件中导出的方法  使用别名来引入，然后再用.来调用方法居然不行？
-import { getHospSetList } from '@/api/hospset'
+import { getHospSetList, deleteHospSet } from '@/api/hospset'
 
 export default {
   // 定义变量和初始值
@@ -14,8 +78,9 @@ export default {
     return {
       current: 1, // 当前页
       limit: 3, // 每页显示记录数
-      searchObj: {},
-      list: [] // 每页数据集合
+      searchObj: {}, // 条件对象
+      list: [], // 每页数据集合
+      total: 0// 总记录数
     }
   },
   created() {
@@ -24,14 +89,36 @@ export default {
   },
   methods: {// 定义方法,请求接口调用
     // 医院设置列表的方法
-    getList() {
+    getList(page = 1) { // page默认为1
+      this.current = page
       getHospSetList(this.current, this.limit, this.searchObj)
         .then(response => { // 请求成功 response是接口返回的数据
+          this.list = response.data.records// 返回集合赋予list
+          this.total = response.data.records.total // 总记录数
           console.log(response)
         })
         .catch(error => { // 请求失败
           console.log(error)
         })
+    },
+    // 删除医院设置的方法
+    removeDataById(id) {
+      // 弹出确认框
+      this.$confirm('此操作将永久删除医院设置, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => { // 确定就执行then方法
+        deleteHospSet(id) // 调用删除接口
+          .then(response => { // 删除接口返回一个删除成功的提示
+            this.$message({
+              type: 'success',
+              message: '删除成功!'
+            })
+            // 刷新页面 即再查询一次列表，默认查第一页
+            this.getList()
+          })
+      })
     }
 
   }
