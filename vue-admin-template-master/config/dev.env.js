27f51995bffc8cf15d9b0d5666bbc1e975346ecc
HealUp <1571826275@qq.com>
2023-03-20 22:54:07 +0800

    add：配置网关8088

--- vue-admin-template-master/config/dev.env.js
+++ vue-admin-template-master/config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"'
-  // nginx地址 改成网关地址
-  BASE_API: '"http://localhost:8203"'
+  // nginx地址 改成网关地址8203
+  BASE_API: '"http://localhost:8088"'
 })
 
